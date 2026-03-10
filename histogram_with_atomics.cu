a7318d4e09905f4e537e039269e6abd9be7113c8
JaimeFine <jaimefine6@gmail.com>
2026-03-10 09:17:39 +0800

    A bad self attempt

--- histogram_with_atomics.cu
+++ histogram_with_atomics.cu
@@ -14,3 +14,24 @@ Problem
     Each thread should process one element from the input array and
     increment the correcponding histogram bin.
 */
+#include <iostream>
+#include <vector>
+#include <cuda_runtime_api.h>
+
+__global__ void histogram(float* X, float* result) {
+    int workIndex = threadIdx.x + blockIdx.x * blockDim.x;
+    X[workIndex] = val;
+    atomicAdd(result[val], 1);
+}
+
+int main() {
+    float* X;
+    float* result;
+
+    X = [1, 3, 2, 1, 0, 2, 3, 1];
+    result = [];
+
+    for (int i = 0; i < B; i++) {
+        std::cout << result[i] << " ";
+    }
+}
