51049a7a2def1ecf540b54ffac6968f6d7014d77
JaimeFine <jaimefine6@gmail.com>
2026-03-23 10:45:19 +0800

    Blind attempt

--- shared_memory_transpose.cu
+++ shared_memory_transpose.cu
@@ -24,3 +24,58 @@ Problem
 
     You should also consider padding shared memory to avoid bank conflicts.
 */
+#include <cuda_runtime_api.h>
+#include <iostream>
+
+__global__ void transpose(float* A, float* B, int M, int N) {
+    extern __shared__ float sharedA[];
+    extern __shared__ float sharedB[];
+
+    int tid = threadIdx.x;
+    int i = blockIdx.x * blockDim.x + tid
+
+    sharedA[tid] = (i < M * N) ? A[i] : 0.0f;
+    sharedB[tid] = (i < M * N) ? B[i] : 0.0f;
+    __syncthreads();
+
+    for (int step = 0; step < M * N; step++) {
+        n = step / M;
+        m = step % N;
+        output = n * N + m;
+        A[step] = B[output];
+        __syncthreads();
+    }
+
+}
+
+int main() {
+    float* A = nullptr;
+    float* B = nullptr;
+
+    int M = 50;
+    int N = 25;
+
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+
+    cudaMallocManaged(&A, M * N * sizeof(float));
+    cudaMallocManaged(&B, M * N * sizeof(float));
+
+    for (int i = 0; i < M * N; i++) {
+        A[i] = i;
+    }
+
+    transpose<<<blocks, threads, threads * sizeof(float)>>>(
+        A, B, M, N
+    );
+    cudaDeviceSynchronize();
+
+    for (int i = 0; i < M * N; i++) {
+        std::cout << B[i] << " ";
+    }
+
+    cudaFree(A);
+    cudaFree(B);
+
+    return 0;
+}
