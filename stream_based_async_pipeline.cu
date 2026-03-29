0b688de1196c9e6155b4b8e644ee7b03ee5d904f
JaimeFine <jaimefine6@gmail.com>
2026-03-29 11:26:49 +0800

    Whatever I tried

--- stream_based_async_pipeline.cu
+++ stream_based_async_pipeline.cu
@@ -25,3 +25,49 @@ Requirements
     - Ensure proper synchronization before accessing final results
     - Divide the workload into chunks per stream
 */
+#include <iostream>
+#include <cuda_runtime_api.h>
+
+__global__ void kernel(float* X, float* Y, int N) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i < N)
+        Y[i] = X[i] * 2.0f;
+}
+
+int main() {
+    float* X = nullptr;
+    float* Y = nullptr;
+    int N = 50;
+
+    int chunck = 10;
+    int num = N / chunk;
+
+    cudaStream_t streams[num];
+
+    for (int i = 0; i < num; i++)
+        cudaStreamCreate(&streams[i])
+
+    cudaMallocHost(&X, N * sizeof(float));
+    cudaMallocHost(&Y, N * sizeof(float));
+
+    float* d_X[num];
+    float* d_Y[num];
+
+    for (int i = 0; i < num; i++) {
+        cudaMalloc(&d_X[i], chunk * sizeof(float));
+        cudaMalloc(&d_Y[i], chunk * sizeof(float));
+    }
+
+    for (int i = 0; i < chunks; i++) {
+        cudaMemcpyAsync(d_X[s], somehting something);
+
+        kernel<<<blocks, threads, 0, streams[s]>>>(d_X[s], d_Y[s], chunk);
+
+        cudaMemcpyAsync(something, something);
+    }
+
+    cudaDeviceSynchronize();
+
+    Free(this);
+    Free(that);
+}
